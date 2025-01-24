a22a7fd0344e5a2f5e13ccdf42748acdda5c8d0c
k022g0114@g.neec.ac.jp <k022g0114@g.neec.ac.jp>
2025-01-24 23:14:39 +0900

    プロジェクト ファイルを追加します。

---
+++ Server.cpp
@@ -0,0 +1,118 @@
+#include <Windows.h>
+#include <WinUser.h>
+#include "resource.h"
+
+// グローバル変数
+const char* title = "Server";
+HWND hwnd;
+WNDCLASS wndclass;
+RECT wrc;
+
+// プロトタイプ宣言
+void MyRegisterClass(HINSTANCE);
+void SettingWindowSize();
+void CreateAndShowWindow(HINSTANCE, int);
+LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
+
+
+/// <summary>
+/// エントリーポイント : WinMain
+/// </summary>
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
+	LPSTR lpCmdLine, int nCmdShow)
+{
+	/* ウィンドウクラスを登録する */
+	MyRegisterClass(hInstance);
+
+	/* ウィンドウサイズを決める */
+	SettingWindowSize();
+
+	/* ウィンドウを生成して表示 */
+	CreateAndShowWindow(hInstance, nCmdShow);
+
+	/* ループ */
+	HACCEL hAccelTable = LoadAccelerators(hInstance, MAKEINTRESOURCE(IDC_DEMO));
+	MSG msg;
+	while (GetMessage(&msg, nullptr, 0, 0))
+	{
+		if (!TranslateAccelerator(msg.hwnd, hAccelTable, &msg))
+		{
+			TranslateMessage(&msg);
+			DispatchMessage(&msg);
+		}
+	}
+
+	return 0;
+}
+
+
+/// <summary>
+/// ウィンドウの仕様&登録
+/// </summary>
+void MyRegisterClass(HINSTANCE hInstance)
+{
+	wndclass.style = CS_HREDRAW | CS_VREDRAW;
+	wndclass.lpfnWndProc = WndProc;
+	wndclass.cbClsExtra = 0;
+	wndclass.cbWndExtra = 0;
+	wndclass.hInstance = hInstance;
+	wndclass.hIcon = LoadIcon(NULL, IDI_APPLICATION);
+	wndclass.hCursor = LoadCursor(NULL, IDC_ARROW);
+	wndclass.hbrBackground = (HBRUSH)GetStockObject(WHITE_BRUSH);
+	wndclass.lpszMenuName = NULL;
+	wndclass.lpszClassName = "Server";
+
+	// ウィンドウクラスを登録する
+	RegisterClass(&wndclass);
+}
+
+
+/// <summary>
+/// ウィンドウサイズを決める
+/// </summary>
+void SettingWindowSize()
+{
+	wrc = { 0, 0, 640, 640 };
+	// クライアント領域をもとに実際のサイズにwrcを変更してもらう
+	AdjustWindowRect(&wrc, WS_OVERLAPPEDWINDOW, false);
+}
+
+
+/// <summary>
+/// ウィンドウを生成して表示
+/// </summary>
+void CreateAndShowWindow(HINSTANCE hInstance, int nCmdShow)
+{
+	hwnd = CreateWindow(
+		wndclass.lpszClassName, title,
+		WS_OVERLAPPEDWINDOW, CW_USEDEFAULT, CW_USEDEFAULT,
+		wrc.right - wrc.left, wrc.bottom - wrc.top,
+		NULL, NULL, hInstance, NULL);
+
+	// ウインドウ表示
+	ShowWindow(hwnd, nCmdShow);
+
+	// ウィンドウ領域更新(WM_PAINTメッセージを発行)
+	UpdateWindow(hwnd);
+}
+
+
+/// <summary>
+/// ウィンドウプロシージャ
+/// </summary>
+LRESULT CALLBACK WndProc(HWND hwnd, UINT iMsg, WPARAM wParam, LPARAM lParam)
+{
+	// メッセージが来てたら最優先で処理させる
+	switch (iMsg) {
+
+		// ウィンドウが破棄された
+	case WM_DESTROY:
+
+		// OSに対して、アプリ終了を伝える
+		PostQuitMessage(0);
+		return 0;
+	}
+
+	// 標準メッセージ処理を行う
+	return DefWindowProc(hwnd, iMsg, wParam, lParam);
+}
